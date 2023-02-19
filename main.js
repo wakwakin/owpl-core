856a01c02f5c6f9e129b2cbe30b44df6579cea1b
wakwakin <guese.joaquin.p.0546@gmail.com>
2023-02-19 19:40:27 +0800

    cors all allow

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ app.use(cors({
     origin: '*'
 }))
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://51.79.243.157/')
+    res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
     res.setHeader('Access-Control-Allow-Credentials', true)
