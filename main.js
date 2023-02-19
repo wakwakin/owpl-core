6147550dca4a16d0878f10aa10e2ab7375e22494
wakwakin <guese.joaquin.p.0546@gmail.com>
2023-02-19 20:25:23 +0800

    add /api

--- main.js
+++ main.js
@@ -22,8 +22,8 @@ app.use((req, res, next) => {
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(Authentication)
-app.use(Member)
+app.use('/api', Authentication)
+app.use('/api', Member)
 
 require('./services/import')
 
